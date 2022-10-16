aa6ef8b3d21a9b3eac209b4ea18b21536b85eae8
TomCalde <tomascalderon@ug.uchile.cl>
2022-10-16 19:59:53 -0300

    init

--- app.js
+++ app.js
@@ -15,18 +15,41 @@ client.on("qr", (qr) => {
   app.get('/get-qr', (req, res) => {
     res.send({qr});
   });
+  console.log("SEND");
 });
 
 client.on("ready", () => {
+
   console.log("ready");
 });
 
 client.initialize();
 
-client.on('message', message => {
+client.on('message', async message => {
   // TODO: send message to ai model and return sth indicating if there 
   // is or not hate speech, or percentage idk
-  console.log("received msg: " + message);
+  try{
+    const contact = await message.getContact();
+    const name = contact.pushname
+    const number = contact.number
+    const chat = await message.getChat();
+    console.log("received msg from number: "+ number + "/ chat name: "+ chat.name+ "/ user name: " + name + "/ msg: " + message.body);
+  }catch{
+    console.log("none");
+  }
+
+});
+
+client.on('message_create', (message) => {
+  // Fired on all message creations, including your own
+  if (message.fromMe) {
+    message.
+    console.log("send msg: " + message.body);
+  }
+});
+
+client.on('disconnected', (reason) => {
+  console.log('Client was logged out', reason);
 });
 
 const PORT = require("process").env.PORT || 3000;
