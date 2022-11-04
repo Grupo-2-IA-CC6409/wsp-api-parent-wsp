469d667691ca1f8173340457dd0f3770a5cbbc49
Gabriel Rojas <gabo06123@gmail.com>
2022-11-04 18:58:22 -0300

    feat: send messages to the model api

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 const express = require('express');
+const axios = require('axios');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 require('dotenv').config();
 
 const app = express();
 
-const cliendId = "test";
+axios.defaults.baseURL = require("process").env.MODEL_API_URL || "127.0.0.1:5000";
+console.log(axios.defaults.baseURL);
+
+const clientId = "test";
 const client = new Client({
-  authStrategy: new LocalAuth({ cliendId: cliendId})
+  authStrategy: new LocalAuth({ clientId: clientId })
 });
 
-// Returns a base64 that is used to create the qrcode and then login into wsp
+var clientStatus = {"status": "not_connected"};
+app.get('/qr-status', (req, res) => {
+  res.send(clientStatus);
+});
+
+// Returns a str that is used to create the qrcode and then login into wsp
 client.on("qr", (qr) => {
+  console.log(`qr: ${qr}`);
+  console.log(new Date());
   app.get('/get-qr', (req, res) => {
     res.send({qr});
   });
@@ -19,15 +30,21 @@ client.on("qr", (qr) => {
 
 client.on("ready", () => {
   console.log("ready");
+  clientStatus.status = "connected";
+});
+
+
+client.on('message', (msg) => {
+  let msgContent = msg.body;
+  console.log(`received msg: ${msgContent}`);
+  let json = {"message": msgContent};
+  axios.post("/predict", json)
+    .then((response) =>{
+      console.log(`is hate?: ${response.data.prediction.label} ${response.data.prediction.score}`);
+    });
 });
 
 client.initialize();
-
-client.on('message', message => {
-  // TODO: send message to ai model and return sth indicating if there 
-  // is or not hate speech, or percentage idk
-  console.log("received msg: " + message);
-});
 
 const PORT = require("process").env.PORT || 3000;
 app.listen(PORT, () => console.log(`running @ http://localhost:${PORT}`));
