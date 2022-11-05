fe2f84bc08913b7c37d9dfe3697e42129387bcb9
Gabriel Rojas <gabo06123@gmail.com>
2022-11-05 03:49:42 -0300

    feat: working with multiple sessions

--- app.js
+++ app.js
@@ -1,50 +1,84 @@
 const express = require('express');
+const cors = require('cors');
 const axios = require('axios');
+const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid');
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
 require('dotenv').config();
 
+axios.defaults.baseURL = require("process").env.MODEL_API_URL;
+
+const ALLOWED_HOSTS = require("process").env.ALLOWED_HOSTS;
+
 const app = express();
+app.use(cors({
+  origin: ALLOWED_HOSTS
+}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
 
-axios.defaults.baseURL = require("process").env.MODEL_API_URL || "127.0.0.1:5000";
-console.log(axios.defaults.baseURL);
+const clients = new Map();
 
-const clientId = "test";
-const client = new Client({
-  authStrategy: new LocalAuth({ clientId: clientId })
+// get qr code
+app.post('/qr-code', (req, res) => {
+  var clientId = req.body.clientId;
+  var context = clients.get(clientId);
+  if (context) {
+    res.send({qr: context.qr});
+  }
 });
 
-var clientStatus = {"status": "not_connected"};
-app.get('/qr-status', (req, res) => {
-  res.send(clientStatus);
+// get qr code status
+app.post('/qr-status', (req, res) => {
+  var clientId = req.body.clientId;
+  var context = clients.get(clientId);
+  if (context) {
+    res.send({status: context.status});
+  } else {
+    res.send({status: "timedout"});
+  }
 });
 
-// Returns a str that is used to create the qrcode and then login into wsp
-client.on("qr", (qr) => {
-  console.log(`qr: ${qr}`);
-  console.log(new Date());
-  app.get('/get-qr', (req, res) => {
-    res.send({qr});
+// create new session
+app.get('/qr-new', (req, res) => {
+  const clientId = uuidv4();
+  const client = new Client({
+    authStrategy: new LocalAuth({ clientId: clientId })
   });
+
+  var context = {client: client, qr: "", qr_timeouts: 0, status: "not_connected"};
+  clients.set(clientId, context);
+
+  client.on("qr", (qr) => {
+    context.qr = qr;
+    if (context.qr_timeouts >= 3) {
+      clients.delete(clientId);
+      context.cliend.destroy();
+    }
+    context.qr_timeouts++;
+  });
+
+  client.on("ready", () => {
+    context.status="connected";
+  });
+
+  client.on('message', (msg) => {
+    var msgContent = msg.body;
+    console.log(`received msg: ${msgContent}`);
+    var json = {"message": msgContent};
+    axios.post("/predict", json)
+      .then((response) =>{
+        console.log(`is hate?: ${response.data.prediction.label} ${response.data.prediction.score}`);
+      });
+  });
+
+  client.initialize();
+
+  res.send({clientId});
 });
-
-client.on("ready", () => {
-  console.log("ready");
-  clientStatus.status = "connected";
-});
-
-
-client.on('message', (msg) => {
-  let msgContent = msg.body;
-  console.log(`received msg: ${msgContent}`);
-  let json = {"message": msgContent};
-  axios.post("/predict", json)
-    .then((response) =>{
-      console.log(`is hate?: ${response.data.prediction.label} ${response.data.prediction.score}`);
-    });
-});
-
-client.initialize();
 
 const PORT = require("process").env.PORT || 3000;
 app.listen(PORT, () => console.log(`running @ http://localhost:${PORT}`));
