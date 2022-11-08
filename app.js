5708cbe483868f4e82c64e2c55eeabc24993dbd8
Gabriel Rojas <gabo06123@gmail.com>
2022-11-08 19:45:24 -0300

    feat: initialize existing sessions on startup

--- app.js
+++ app.js
@@ -4,14 +4,16 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 const { Client, LocalAuth } = require('whatsapp-web.js');
+const fs = require('fs');
 
 require('dotenv').config();
 
-axios.defaults.baseURL = require("process").env.MODEL_API_URL;
+axios.defaults.baseURL = require('process').env.MODEL_API_URL;
 
-const ALLOWED_HOSTS = require("process").env.ALLOWED_HOSTS;
+const ALLOWED_HOSTS = require('process').env.ALLOWED_HOSTS;
 
 const app = express();
+
 app.use(cors({
   origin: ALLOWED_HOSTS
 }));
@@ -21,6 +23,17 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 const clients = new Map();
+
+// initialize existing session on startup
+const folder = '.wwebjs_auth';
+fs.readdirSync(folder).forEach(file => {
+  // really shity code, not to use irl
+  var clientId = file.substring(8);
+  var client = new Client({
+    authStrategy: new LocalAuth({ clientId: clientId }),
+  });
+  client.initialize();
+});
 
 // get qr code
 app.post('/qr-code', (req, res) => {
@@ -38,7 +51,7 @@ app.post('/qr-status', (req, res) => {
   if (context) {
     res.send({status: context.status});
   } else {
-    res.send({status: "timedout"});
+    res.send({status: 'timedout'});
   }
 });
 
@@ -47,15 +60,12 @@ app.get('/qr-new', (req, res) => {
   const clientId = uuidv4();
   const client = new Client({
     authStrategy: new LocalAuth({ clientId: clientId }),
-    puppeteer: {
-      args: ['--no-sandbox'],
-    }
   });
 
-  var context = {client: client, qr: "", qr_timeouts: 0, status: "not_connected"};
+  var context = {client: client, qr: '', qr_timeouts: 0, status: 'not_connected'};
   clients.set(clientId, context);
 
-  client.on("qr", (qr) => {
+  client.on('qr', (qr) => {
     context.qr = qr;
     if (context.qr_timeouts >= 3) {
       clients.delete(clientId);
@@ -64,15 +74,15 @@ app.get('/qr-new', (req, res) => {
     context.qr_timeouts++;
   });
 
-  client.on("ready", () => {
-    context.status="connected";
+  client.on('ready', () => {
+    context.status='connected';
   });
 
   client.on('message', (msg) => {
     var msgContent = msg.body;
     console.log(`received msg: ${msgContent}`);
-    var json = {"message": msgContent};
-    axios.post("/predict", json)
+    var json = {'message': msgContent};
+    axios.post('/predict', json)
       .then((response) =>{
         console.log(`is hate?: ${response.data.prediction.label} ${response.data.prediction.score}`);
       });
@@ -83,5 +93,5 @@ app.get('/qr-new', (req, res) => {
   res.send({clientId});
 });
 
-const PORT = require("process").env.PORT || 3000;
+const PORT = require('process').env.PORT || 3000;
 app.listen(PORT, () => console.log(`running @ http://localhost:${PORT}`));
